ec95dd86b3cab0f99a09af959f49cd3772eb00f4
Nhan Hoang <skynetx002@gmail.com>
2017-05-22 14:12:04 +0700

    bo sung portlet 114 manage notification

---
+++ docroot/WEB-INF/src/org/opencps/notification/portlet/ManagePortlet.java
@@ -0,0 +1,22 @@
+package org.opencps.notification.portlet;
+
+import com.liferay.portal.kernel.struts.BaseStrutsPortletAction;
+
+
+
+public class ManagePortlet extends BaseStrutsPortletAction{
+	
+//	@Override
+//	public void processAction(StrutsPortletAction originalStrutsPortletAction,
+//	       PortletConfig portletConfig, ActionRequest actionRequest,
+//	       ActionResponse actionResponse) throws Exception {
+//
+//	System.out.println("____CutomStrutsPortletAction processAction");
+//	             
+//	originalStrutsPortletAction.processAction(originalStrutsPortletAction, portletConfig, actionRequest,actionResponse);
+//	       }
+
+	
+}
+	
+	
